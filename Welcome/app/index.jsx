63241f93d9b990ecca28631d8b77af286e5b3492
Prashant Chauhan <psc856@gmail.com>
2025-01-07 19:04:20 +0530

    Link Button created

--- Welcome/app/index.jsx
+++ Welcome/app/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet,ImageBackground} from 'react-native'
+import { View, Text, StyleSheet,ImageBackground,Pressable} from 'react-native'
 import React from 'react'
 
 import IcedCoffeeImg from "@/assets/images/iced-coffee.png"
@@ -13,7 +13,13 @@ const app = () => {
       style={styles.image}
       >
       <Text style={styles.title}>Hey Prashant</Text>
-      <Link href="/explore" style={styles.link}>Explore</Link>
+      <Link href="/explore" style={{marginHorizontal: 'auto'}} asChild>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>
+          Explore
+        </Text>
+      </Pressable>
+      </Link>
       </ImageBackground>
     </View>
   )
@@ -48,6 +54,20 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     textDecorationLine:'underline',
     backgroundColor:'rgba(0,0,0,0.5)',
+    padding:4,
+  },
+  button:{
+    height:60,
+    borderRadius:20,
+    justifyContent:'center',
+    backgroundColor:'rgba(0,0,0,0.75)',
+    padding:6,
+  },
+  buttonText:{
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
     padding:4,
   }
 })
